f57d79c66c6af946854c84a80f3ba5580ced5124
Amine Amari <amine.amari@web.de>
2022-11-10 21:03:10 +0100

    implemented special menu

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({title, price, tags}) => (
+  <div className="app__menuitem">
+    <div className="app__menuitem-head">
+      <dir className="app__menuitem-name">
+        <p className="p__cormorant" style={{color: '#DCCA87'}}> {title}</p>
+      </dir>
+
+      <div className="app__menuitem-dash"></div>
+      
+      <dir className="app__menuitem-price">
+        <p className="p__cormorant"> {price}</p>
+      </dir>
+    </div>
+
+    <dir className="app__menuitem-sub">
+      <p className="p__opensans" style={{color: '#AAA'}}>{tags}</p>
+    </dir>
   </div>
 );
 
